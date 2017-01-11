d1f27b0ee5a96795e1d7079d251904f8e1a4e453
mpokryva <mpokryva@gmail.com>
2017-01-11 01:00:58 -0500

    Added pin verification.

--- app/src/main/java/com/example/miki/alexasmschecker/SetUpActivity.java
+++ app/src/main/java/com/example/miki/alexasmschecker/SetUpActivity.java
@@ -11,8 +11,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SetUpActivity extends AppCompatActivity {
 
@@ -21,9 +24,10 @@ public class SetUpActivity extends AppCompatActivity {
     private Button confirmButton;
 
     // Write a message to the database
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference rootRef = database.getReference();
-    DatabaseReference newPinsRef = rootRef.child("newPins");
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference rootRef = database.getReference();
+    private DatabaseReference newPinsRef = rootRef.child("newPins");
+    private DatabaseReference verificationQueueRef = rootRef.child("verificationQueue");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,34 +42,54 @@ public class SetUpActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String pinString = pinEditText.getText().toString();
                 String processedPin = "";
-                String phoneNumber = phoneNumEditText.getText().toString();
+                final String phoneNumber = phoneNumEditText.getText().toString();
                 //Convert characters to lowercase (just in case).
                 for (int i = 0; i < pinString.length(); i++) {
                     processedPin += Character.toLowerCase(pinString.charAt(i));
                 }
                 // Check that phone number is valid.
-                if (PhoneNumberUtils.isGlobalPhoneNumber(phoneNumber)){
-                    String processedNumber="";
-                    for (int i = 0; i < phoneNumber.length(); i++){
+                if (PhoneNumberUtils.isGlobalPhoneNumber(phoneNumber)) {
+                    String processedNumber = "";
+                    for (int i = 0; i < phoneNumber.length(); i++) {
                         // If it is positive, then it is a number.
-                        if (Character.getNumericValue(phoneNumber.charAt(i)) > 0){
+                        if (Character.getNumericValue(phoneNumber.charAt(i)) > 0) {
                             processedNumber += phoneNumber.charAt(i);
                         }
                     }
-                    newPinsRef.child(processedPin + "/phoneNumber").setValue(processedNumber);
-                }
-                else {
+                    final DatabaseReference processedPinRef = newPinsRef.child(processedPin);
+                    final String processedNumToSend = processedNumber;
+                    final String pinFinal = processedPin;
+                    processedPinRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                processedPinRef.removeValue();
+                                rootRef.child("users").child(pinFinal).child("phoneNumber").setValue(processedNumToSend);
+                                Toast.makeText(SetUpActivity.this, "Pin matched. You're ready to go!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(SetUpActivity.this, "Pin does not exist.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                } else {
                     Toast.makeText(SetUpActivity.this, "Invalid phone number. Try again.",
                             Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
+    }
 
-
-
+    private void addToVerificationQueue(String phoneNumber){
 
     }
+
+
 
 
 }
