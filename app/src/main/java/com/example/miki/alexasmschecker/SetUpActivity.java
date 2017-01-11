7b04be8c1971157fd2d965925c51a1b26cb5fea6
mpokryva <mpokryva@gmail.com>
2017-01-10 22:30:58 -0500

    Added phone number verification.

--- app/src/main/java/com/example/miki/alexasmschecker/SetUpActivity.java
+++ app/src/main/java/com/example/miki/alexasmschecker/SetUpActivity.java
@@ -2,9 +2,14 @@ package com.example.miki.alexasmschecker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
+import android.text.TextUtils;
+import android.text.method.CharacterPickerDialog;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -12,10 +17,13 @@ import com.google.firebase.database.FirebaseDatabase;
 public class SetUpActivity extends AppCompatActivity {
 
     private EditText pinEditText;
+    private EditText phoneNumEditText;
     private Button confirmButton;
+
     // Write a message to the database
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
+    DatabaseReference rootRef = database.getReference();
+    DatabaseReference newPinsRef = rootRef.child("newPins");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +31,41 @@ public class SetUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setup);
         pinEditText = (EditText) findViewById(R.id.edittext_pin);
         confirmButton = (Button) findViewById(R.id.confirm_button);
+        phoneNumEditText = (EditText) findViewById(R.id.edittext_phone_num);
 
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String editText = pinEditText.getText().toString();
-                myRef.setValue(editText);
+                String pinString = pinEditText.getText().toString();
+                String processedPin = "";
+                String phoneNumber = phoneNumEditText.getText().toString();
+                //Convert characters to lowercase (just in case).
+                for (int i = 0; i < pinString.length(); i++) {
+                    processedPin += Character.toLowerCase(pinString.charAt(i));
+                }
+                // Check that phone number is valid.
+                if (PhoneNumberUtils.isGlobalPhoneNumber(phoneNumber)){
+                    String processedNumber="";
+                    for (int i = 0; i < phoneNumber.length(); i++){
+                        // If it is positive, then it is a number.
+                        if (Character.getNumericValue(phoneNumber.charAt(i)) > 0){
+                            processedNumber += phoneNumber.charAt(i);
+                        }
+                    }
+                    newPinsRef.child(processedPin + "/phoneNumber").setValue(processedNumber);
+                }
+                else {
+                    Toast.makeText(SetUpActivity.this, "Invalid phone number. Try again.",
+                            Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
+
+
+
+
     }
+
+
 }
